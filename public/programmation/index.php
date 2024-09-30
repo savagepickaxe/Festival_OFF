06385b65431d47d14a7d66317ca9fdaa7d313c0c
El Hadj Papa Magor Sow <Papamagorion@gmail.com>
2024-09-30 13:13:55 -0400

    Small Modifications

--- public/programmation/index.php
+++ public/programmation/index.php
@@ -1,4 +1,27 @@
-Programmation!
 
 <?php $niveau="../";?>
 <a href="<?php echo $niveau;?>index.php">Retour</a>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <title>Festival OFF - Programmation</title>
+</head>
+
+<body>
+    <header>
+    <?php include($niveau . "liaisons/fragments/entete.inc.php") ?>
+    </header>
+    <main>
+        <h1>
+            Programmation
+        </h1>
+    </main>
+    <footer>
+        <?php include($niveau . "liaisons/fragments/piedDePage.inc.php") ?>
+
+    </footer>
+
+</body>
+</html>
