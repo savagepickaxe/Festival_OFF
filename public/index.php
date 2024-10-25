dd6456ecd30139febdfe80a2fa40312dc48dd544
Esteban Henry <2364488@csfoy.ca>
2024-10-25 09:37:45 -0400

    scss

--- public/index.php
+++ public/index.php
@@ -1,16 +1,91 @@
 
 <?php $niveau = "";
-/*
-			$requeteSQL="Select titre from actualites";
-			$objStat=$objPdo -> prepare($requeteSQL);
-			$objStat -> execute();
-			$arrActualite=$objStat -> fetchAll();
-			forEach($arrActualite as $actualite){
-				echo $actualite["titre"];?><BR>
-			<?php } 
-			*/
-?> 
 
+		
+?> 
+<?php 
+	//---------------------------------- Variables importantes ----------------------------------//
+	//Gestion des jours et des mois
+	$arrJours = array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
+	$arrMois = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+
+    //---------------------------------- Connexion à la base de données ----------------------------------//
+
+    $niveau="./";
+
+    include($niveau . 'liaisons/php/config.inc.php');
+
+
+
+
+	//----------------------------------------------------------------------------------------------------//
+	//Requête pour obtenir les trois à 5 derniers articles
+	$intNbArticlesAffichage = rand(3, 5);
+	$strRequeteActualites="SELECT id, titre, auteurs, date_actualite AS date_complete,
+						YEAR(date_actualite) AS annee, 
+						MONTH(date_actualite) AS mois, 
+						DAYOFMONTH(date_actualite) AS jour, 
+						HOUR(date_actualite) AS heure, 
+						MINUTE(date_actualite) AS minute, 
+						article FROM actualites ORDER BY date_actualite DESC LIMIT 0, " . $intNbArticlesAffichage;
+
+	
+
+
+	$objResultatArticles = $pdoConnexion -> query($strRequeteActualites);
+
+	$arrArticles = array();
+
+	for($i=0; $ligne = $objResultatArticles->fetch(); $i++){
+		$arrArticles[$i]['id'] = $ligne['id'];
+		$arrArticles[$i]['titre'] = $ligne['titre'];
+		$arrArticles[$i]['auteurs'] = $ligne['auteurs'];
+		$arrArticles[$i]['date_complete'] = $ligne['date_complete'];
+		$arrArticles[$i]['annee'] = $ligne['annee'];
+		$arrArticles[$i]['mois'] = $ligne['mois'];
+		$arrArticles[$i]['jour'] = $ligne['jour'];
+		$arrArticles[$i]['heure'] = $ligne['heure'];
+		$arrArticles[$i]['minute'] = $ligne['minute'];
+
+		$arrContenuArticle = explode(' ', $ligne['article']);
+		if(count($arrContenuArticle) > 45){
+			array_splice($arrContenuArticle, 45, count($arrContenuArticle));
+		}
+		$arrArticles[$i]['article_preview'] = implode(' ', $arrContenuArticle);
+	}
+
+	$objResultatArticles->closeCursor();
+
+	//---------------------------------- Suggestions d'artistes ----------------------------------//
+
+    //Sélection aléatoire de 3 à 5 artistes
+    $nbSuggestions = rand(3, 5);
+
+    //Sélection de tous les artistes
+    $strRequeteSuggestion = "SELECT artistes.nom, artistes.id FROM artistes ORDER BY nom";
+
+    //Récupération des artistes 
+    $pdoResultatSuggestion = $pdoConnexion->query($strRequeteSuggestion);
+    $arrArtistesSuggestionPotentiel = array();
+    for($i = 0; $ligne = $pdoResultatSuggestion->fetch(); $i++){
+        $arrArtistesSuggestionPotentiel[$i]['nom'] = $ligne['nom'];
+        $arrArtistesSuggestionPotentiel[$i]['id'] = $ligne['id'];
+    }
+
+    //Fermeture de la requête
+    $pdoResultatSuggestion->closeCursor();
+
+    //---------------------------------------------------
+    $arrArtistesSuggestion = array();
+    for($i = 0; $i < $nbSuggestions; $i++){
+        //Sélection aléatoire d'un artiste
+        $artisteChoisi = rand(0, count($arrArtistesSuggestionPotentiel) - 1);
+        //Ajout de l'artiste dans le tableau
+        array_push($arrArtistesSuggestion, $arrArtistesSuggestionPotentiel[$artisteChoisi]);
+        //Suppression de l'artiste du tableau potentiel
+        array_splice($arrArtistesSuggestionPotentiel, $artisteChoisi, 1);
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -29,53 +104,78 @@
 		<?php include $niveau . "liaisons/fragments/entete.inc.php"; ?>
 	</header>
 
-	<main>
-<div id="container"></div>
-
-		<div id="contenu" class="conteneur">
+	<main id="contenu" class="conteneur">
 			<section class="accueil">
-			<div class="gros-titre">
+			<header class="gros-titre">
 				<div class="lignes ligne1">
 					<span>Festival</span>
-					<div class="bulle"></div>
 				</div>
 				<div class="lignes ligne2">
 					<span>Off</span>
 			</div>
-			</div>
-			<div class="image-menu">
-				<div class="video-background">
+			</header>
+			<footer class="image-menu">
+				<article class="video-background">
 					<div class="video-tour">
 						<video src="liaisons/videos/OFF.mp4"  
 						width="640" height="360" autoplay  loop muted  frameborder="0" allow="autoplay; encrypted-media"
 						 allowfullscreen>
-							
-</video>
+						</video>
 					</div>
-				</div>
-				<div class="h1-boite">
+				</article>
+				<article class="h1-boite">
 					<div class="gros-titre ">OFF</div>
 					<div class="coin left-top"></div>
 					<div class="coin right-bottom"></div>
-				</div>
-			</div>
-
+				</article>
+			</footer>
 			</section>
+
+
 			
-
-
-
-			<section>
-				<h3>Entête de section</h3>
-				<article>
-			
+		
+			<div id="contenu" class="conteneur">
+			<section class="article">
+				<h2>Articles</h2>
+				<?php foreach($arrArticles as $article){ ?>
+					<article>
+						<header>
+							<h3><?php echo $article['titre'];?></h3>
+						</header>
+						<p>
+							<?php echo $article['article_preview'];?>
+							<a href="#">...</a>
+						</p>
+						<footer>
+							<p> <?php echo $article['auteurs']; ?></p>
+							<time datetime="<?php echo $article['date_complete'];?>">
+								<?php echo "Le " . $article['jour'] . " " . $arrMois[$article['mois'] - 1] . " " . $article['annee'] . " à " . $article['heure'] . "h" . $article['minute'];?>
+							</time>
+						</footer>
+					</article>
+				<?php } ?>
 			</section>
+			<section class="suggestions">
+				<h2>Artistes suggérés</h2>
+				<ul>
+					<?php foreach($arrArtistesSuggestion as $artiste){ ?>
+						<li>
+							<a href="<?php echo $niveau ?>artistes/fiches/index.php?id_artiste=<?php echo $artiste['id']; ?>">
+								<?php echo $artiste['nom']; ?>
+								<!-- Sources des images pas encore définies par l'équipe. Vérification sur le alt -->
+								<img src="#" alt="<?php echo "ID artiste : " . $artiste['id'] ?>">
+							</a>
+						</li>
+					<?php } ?>
+				</ul>
 		</div>
+
+			
+			</section>
+		
 	
    
-        <p><a href="#" class="bouton">Bouton</a></p>
-		<p><a href="#" class="bouton--inverse">Bouton</a></p>
-     <a href="#" class="hyperlien">lien test!</a>
+      
 	</main>
 
 	
